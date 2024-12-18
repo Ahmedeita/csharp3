bbddbcaa6f253c00a619bbc4d4f3becdcd83b320
Ahmedeita <ahmedeita.dev@gmail.com>
2024-12-18 02:45:30 +0200

    1. Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.

--- csharp3/Program.cs
+++ csharp3/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //1. Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+            //Console.Write("Enter Number : ");
+            //int Num = int.Parse(Console.ReadLine());
+
+            //if (Num % 3 == 0 && Num % 4 == 0)
+            //    Console.WriteLine("Yes");
+            //else
+            //    Console.WriteLine("No");
+
+            #endregion
         }
     }
 }
