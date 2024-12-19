a473b029195384547cffdbad422e67bd6fdf7773
Ahmedeita <ahmedeita.dev@gmail.com>
2024-12-19 02:56:49 +0200

    Part 01 Implicit Casting Vs Explicit Casting

--- Demo3/Program.cs
+++ Demo3/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Value Type Casting
+
+            #region Implicit Casting - safe casting
+            //int X =4 ; //4 Bytes
+            //  long Y = X;
+            // long y = /*(long)*/X; //ملهاش لازمة اضيف كاست هنا 
+            //safe casting 
+
+            #endregion
+
+            #region Explicit Casting - Unsafe Casting
+            //long X = 4154255541541541521; // 8 Bytes
+            //int Y;
+            //Checked
+            //{
+            // Y =(int) X; //OverFlow Exception //Runtime Error
+            // }
+            //Console.WriteLine(Y);
+
+            //double X =88.8;
+            //int Y = (int)X;
+            //Console.WriteLine(Y); // will print without Fractions
+
+
+
+            #endregion
+
+
+            #endregion
+
         }
     }
 }
